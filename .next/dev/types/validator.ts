d1fc5d51a4f845c9fb2227eaeb2b633b6d20ad0a
Joaquim Magode <135075390+JoaquimMagode@users.noreply.github.com>
2025-12-23 16:31:39 +0530

    update

--- .next/dev/types/validator.ts
+++ .next/dev/types/validator.ts
@@ -479,6 +479,15 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
   type __Unused = __Check
 }
 
+// Validate ../../../app/api/fix/route.ts
+{
+  type __IsExpected<Specific extends RouteHandlerConfig<"/api/fix">> = Specific
+  const handler = {} as typeof import("../../../app/api/fix/route.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../../app/api/orders/[id]/route.ts
 {
   type __IsExpected<Specific extends RouteHandlerConfig<"/api/orders/[id]">> = Specific
